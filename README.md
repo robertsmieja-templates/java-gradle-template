8f2d25922bc9001ddeeaa287102017d462fb8480
Robert Smieja <robertsmieja@users.noreply.github.com>
2019-10-21 22:00:38 -0400

    Initial commit of Java library example + best practicies

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # java-gradle-template
  Generic Java project template following best practices
+
+## Technology Choices and Reasoning
+- Git Hooks - Automatically run formatting + unit tests on commit.
+- Gradle - Currently the "best" build system due to convention-over-configuration, ability to skip over tasks, and flexibility. (Plus less XML)
+    - Kotlin DSL - Statically typed and better IDE support than Groovy DSL
+    - Spotless Gradle Plugin - Automatically reformats code
+        - Google Java Format - Reasonable default for formatting
+- Jib - Google Container builder that automatically applies best practices for a Docker image, using Distroless images
+- JUnit 5 - Latest version of JUnit with new features, such as an improved Extension API
+- Reckon - Automatically and deterministically get a build version, using the Git repository information 
+- SpotBugs - Static analysis tool to catch errors and follow best-practices
---
+++ build.gradle.kts
@@ -0,0 +1,93 @@
+import org.apache.commons.lang3.SystemUtils
+
+plugins {
+    // Apply the java-library plugin to add support for Java Library
+    `java-library`
+    id("org.ajoberstar.reckon") version "0.11.0"
+    id("com.diffplug.gradle.spotless") version "3.25.0"
+    id("com.github.spotbugs") version "2.0.0"
+    id("com.google.cloud.tools.jib") version "1.7.0"
+    id("com.star-zero.gradle.githook") version "1.2.0"
+}
+
+repositories {
+    // Use jcenter for resolving dependencies.
+    // You can declare any Maven/Ivy/file repository here.
+    jcenter()
+    mavenCentral()
+}
+
+dependencies {
+    // This dependency is exported to consumers, that is to say found on their compile classpath.
+    // api("org.apache.commons:commons-math3:3.6.1")
+
+    spotbugsPlugins("com.h3xstream.findsecbugs:findsecbugs-plugin:1.7.1")
+
+    // This dependency is used internally, and not exposed to consumers on their own compile classpath.
+    implementation("com.google.guava:guava:28.1-jre")
+
+    // Use JUnit Jupiter API for testing.
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.5.2")
+
+    // Use JUnit Jupiter Engine for testing.
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.5.2")
+}
+
+/**
+ * Plugin configuration
+ */
+
+githook {
+    failOnMissingHooksDir = false
+    createHooksDirIfNotExist = true
+
+    // Gradle git-hooks plugin does not handle directories with spaces on Windows
+    if (!SystemUtils.IS_OS_WINDOWS) {
+        hooks {
+            create("pre-commit") {
+                task = "check"
+            }
+        }
+    }
+}
+
+reckon {
+    scopeFromProp()
+    stageFromProp("milestone", "rc", "final")
+}
+
+spotbugs {
+    effort = "max"
+}
+
+spotless {
+    java {
+        // custom("Lambda fix") {
+        //      it.replace("} )", "})").replace("} ,", "},")
+        // }
+
+        endWithNewline()
+        removeUnusedImports()
+        trimTrailingWhitespace()
+
+        googleJavaFormat()
+    }
+}
+
+/**
+ * Task configurations
+ */
+
+val test by tasks.getting(Test::class) {
+    // Use junit platform for unit tests
+    useJUnitPlatform()
+}
+
+val spotlessApply = tasks.spotlessApply
+val spotlessCheck = tasks.spotlessCheck {
+    shouldRunAfter(spotlessApply)
+}
+
+tasks.check {
+    dependsOn(spotlessApply)
+}
